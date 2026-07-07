66b4d5685aaea451b062d468e15a666c0ee69285
Victor Olusegun <teeveeolusegun@gmail.com>
2026-07-07 06:10:59 +0100

    Added readData function to read parking records from data.json

--- utils/parking-records.js
+++ utils/parking-records.js
@@ -1,2 +1,10 @@
 // this is a placeholder for /parking-records utils
 // parking-records helper functions are here.
+
+import fs from 'fs/promises';
+const filePath = './data/data.json';
+
+export async function readData() {
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+}
