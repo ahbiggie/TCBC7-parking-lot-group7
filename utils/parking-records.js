25a96527a76284bef9be610bf12ae134aa90b620
Victor Olusegun <teeveeolusegun@gmail.com>
2026-07-07 06:20:06 +0100

    Added writeData function to save parking records to  data.json

--- utils/parking-records.js
+++ utils/parking-records.js
@@ -8,3 +8,7 @@ export async function readData() {
     const data = await fs.readFile(filePath, 'utf-8');
     return JSON.parse(data);
 }
+
+export async function writeData(data) {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
+}
