3c0b4564114cd0f4ec3e6769acd8ef3f12293506
Victor Olusegun <teeveeolusegun@gmail.com>
2026-07-07 07:14:03 +0100

    Handled writeData function gracefully, and added error message to readData function

--- utils/parking-records.js
+++ utils/parking-records.js
@@ -10,12 +10,17 @@ export async function readData() {
     return JSON.parse(data);
     } catch (error) {
         if (error.code === 'ENOENT') {
-            //'ENOENT' means 'Error NO ENTry' or 'no such file or directory', which means the file does not exist
-            return []; // File does not exist, return an empty array
-        }   throw new Error;
+            //'ENOENT' means 'Error NO ENTry' or 'no such file or directory'.
+            return []; // File does not exist ?? return an empty array
+        }   throw new Error(`Failed to read data: ${error.message}`);
     }
 }
 
 export async function writeData(data) {
-    await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
+    try{
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
+    } catch (error) {
+        console.error('Error writing data:', error);
+        throw new Error(`Failed to write data: ${error.message}`);
+    }
 }
