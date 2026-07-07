9d6f7326a89750ec5e88f63189d63fce22db7f21
shaibu lawal yusuf <ahbiggielawal@yahoo.com>
2026-07-07 17:19:21 +0100

    added readRecords utility function for async JSON file retrieval and documentation

---
+++ utils/recordsStorage.js
@@ -0,0 +1,26 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const recordsPath = path.join(__dirname, '../data', 'records.json');
+
+async function readRecords() {
+    try {
+        //1. read all records from records.json
+        const records = await fs.readFile(recordsPath, 'utf-8');
+        //2. convert records to js arrays
+        const recordsToArray = JSON.parse(records);
+        // 3. return the array
+        return recordsToArray;
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            return [];
+        }
+        throw error;
+    }
+}
+
+console.log(await readRecords());
