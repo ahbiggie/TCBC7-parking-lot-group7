e6a0d23175b37dfd5dfe6ef29cd0ddadad443ea6
DammyDehlian <rufaioluwadamilare76@gamil.com>
2026-07-08 12:15:51 +0100

    Add GET routes for parking records

--- routes/parking-records.js
+++ routes/parking-records.js
@@ -1,2 +1,53 @@
-// this is a placeholder for /parking-records router
-// the router should be able to handle all the parking record requests
+import express from "express";
+import { readRecords } from "../utils/recordsStorage";
+
+const router = express.Router();
+
+//GET all parking records
+router.get("/api/v1/parking-lot-records", async (req, res) => {
+    try {
+        const records = await readRecords();
+        res.status(200).json({
+            status : "success",
+            results : records.length,
+            data : records,
+        });
+    }
+    catch(error) {
+        res.status(500).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+
+});
+
+// GET one parking record
+router.get("/api/v1/parking-lot-records/:lotId", async (req, res) => {
+    try{
+        const { lotId } = req.params;
+        const records = await readRecords();
+        const record = records.find(
+            (record) => record.lotId === lotId
+        );
+
+        if (!record) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Parking record not found",
+            });
+        }
+       res.status(200).json({
+        status: "success",
+        data: record,
+       });
+    }
+    catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+});
+
+export default router;
