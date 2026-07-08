0609ce125a2f38a8c621a684960019d18145290f
Uduak Umanah <eyakenojnr@gmail.com>
2026-07-08 08:34:15 +0100

    [Update]: added parking vehicle logic

--- routes/parkingLots.js
+++ routes/parkingLots.js
@@ -20,21 +20,67 @@ async function writeData(data) {
 }
 
 // PUT endpoint to park a vehicle in a specific slot
-router.put('/:lotId', (req, res) => {
-    // Get lot ID from url & vehicle details from request body
-    const { lotId } = req.params;
-    const { licensePlate, vehicleType } = req.body;
+router.put('/:lotId', async (req, res) => {
+    try {
+        // Get lot ID from url & vehicle details from request body
+        const { lotId } = req.params;
+        const { licensePlate, vehicleType } = req.body;
 
-    console.log(lotId, licensePlate, vehicleType);
+        if (!licensePlate || !vehicleType) {
+            return res.status(400).json({
+                error: "license and vehicleType are required to park a vehicle."
+            });
+        }
 
-    // send test response to see if server receives the data
-    res.json({
-        message: `Attempting to park in slot ${lotId}`,
-        data: {
-            licensePlate,
-            vehicleType,
-        },
-    });
+        const parkingLots = await readData();
+
+        // Find the specific parking slot in JSON file
+        const lot = parkingLots.find(item => item.lotId === lotId);
+
+        // If slot deosn't exist...
+        if (!lot) {
+            return res.status(404).json({
+                error: `Parking slot with ID ${lotId} does not exist.`
+            });
+        }
+
+        // If slot is already occupied...
+        if (lot.status === 'parked') {
+            return res.status(409).json({
+                error: `Parking slot ${lotId} is already occupied by a vehicle with license plate ${lot.licensePlate}.`
+            });
+        }
+
+        // Update parking slot details with newly parked vehicle info
+        lot.licensePlate = licensePlate;
+        lot.vehicleType = vehicleType;
+        lot.status = 'parked';
+
+        const now = new Date().toISOString();
+        lot.updatedAt = now;
+        lot.parkedAt = now;
+        lot.unparkedAt = null;
+
+        await writeData(parkingLots);
+        return res.status(200).json({
+            message: "Vehicle parked successfully.",
+            data: lot
+        });
+
+        // send test response to see if server receives the data
+        res.json({
+            message: `Attempting to park in slot ${lotId}`,
+            data: {
+                licensePlate,
+                vehicleType,
+            },
+        });
+    } catch (error) {
+        console.error("Error while handling PUT /:ilotId :", error);
+        return res.status(500).json({
+            error: "Unexpected internal server error occurred."
+        });
+    }
 });
 
 export default router;
